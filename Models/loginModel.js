6e9f4c55c5e52ab0ab2da2533d7dacee4dc7d52b
Caleb <S10267482@connect.np.edu.sg>
2025-06-26 18:21:35 +0800

    b4 login model

---
+++ Models/loginModel.js
@@ -0,0 +1,10 @@
+const sql = require('mssql');
+const dbConfig = require('../config/dbConfig');
+
+async function findUserByEmail(email) {
+  await sql.connect(dbConfig);
+  const result = await sql.query`SELECT * FROM Users WHERE email = ${email}`;
+  return result.recordset[0];
+}
+
+module.exports = { findUserByEmail };
---
+++ signup.js
@@ -0,0 +1,2 @@
+const API_KEY = '68540de5bb5ccc6841f6d94c';
+const DB_URL = 'https://lionbef-66bb.restdb.io/rest/login';
