f10e752bdb7735dc3fe1348a4458fc5b912dcba7
Caleb <S10267482@connect.np.edu.sg>
2025-06-30 16:13:30 +0800

    m

--- Models/medsModel.js
+++ Models/medsModel.js
@@ -2,3 +2,18 @@ const sql = require('mssql');
 const dbConfig = require('../config/dbConfig');
 
 //GET//
+async function GetAllDates(){
+    let connection;
+    try{
+        connection = await sql.connect(dbConfig)
+        const query "SELECT id, medicine, datetime FROM Medicine";
+
+    }
+}
+
+//CREATE//
+async function CreateDate(){
+    let connection;
+
+
+}
