2a6c0c639bc762ceb714d43288a6804d2b238846
Caleb <S10267482@connect.np.edu.sg>
2025-07-03 14:05:50 +0800

    medsmodel

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const medsRoutes = require('./routes/medsRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware//
+app.use(express.json()); // 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/meds', medsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+//ROUTES//
+
+
+//Start + Shutdown server//
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+process.on("SIGINT", async () => {
+  console.log("Server is gracefully shutting down");
+  await sql.close();
+  console.log("Database connections closed");
+  process.exit(0);
+});
+
+module.exports = app;
