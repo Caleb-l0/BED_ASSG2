55be141b5eb88f5b491a69825ea99db728006937
Caleb <S10267482@connect.np.edu.sg>
2025-07-06 17:33:12 +0800

    import jwt and validadtions for login/signup

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -1,7 +1,14 @@
-const loginModel = require("../Models/loginModel");
+const jwt = require('jsonwebtoken');
+const loginModel = require('../Models/loginModel');
 
-async function findUserByEmail(res,req){
+async function login(req, res) {
+  const { email, password } = req.body;
+  const user = await loginModel.findUserByEmail(email);
 
+  if (!user || user.password !== password) {
+    return res.status(401).json({ error: 'Invalid email or password' });
+  }
+
+  const token = jwt.sign({ userId: user.id, email: user.email }, 'mysecretkey');
+  res.json({ token });
 }
-
-module.exports = {findUserByEmail}
---
+++ Middlewares/loginValidation.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
+
+function validateLogin(req,res,next){
+    const { error } = loginSchema.validate(req.body);
+    if (error){
+        return res.status(400).json({ error: errorMessage });
+    }
+    next()
+}
+
+module.exports = 
+{
+    validateLogin,
+    
+
+}
